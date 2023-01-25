1f0e5dc1f4dea53a03571a7d232c54e4da521077
Miguel Bermudo <mig.ber.bay@gmail.com>
2023-01-25 10:09:52 +0000

    added descriptions to cache generation function

--- misc/generate_cache_for_dataset.py
+++ misc/generate_cache_for_dataset.py
@@ -1,19 +1,29 @@
 import networkx as nx
 from pathlib import Path
 from tqdm import tqdm
-import os
+import os, pickle
 
 
 current_dir = Path(__file__).parent.resolve()
 parent_dir = current_dir.parent.resolve()
 dataset_path = Path(f"{parent_dir}/datasets").resolve()
+cache_dir = Path(f"{parent_dir}/model/data/caches").resolve()
 
 datasets = [name for name in os.listdir(dataset_path) if os.path.isdir(f"{dataset_path}/{name}")]
 
 
-def main():
+def main(exclude, max_depth):
+    """
+    Generates distance cache for all datasets excepts the ones indicated.
+
+    :param exclude: datasets to ignore when generating caches.
+    :param max_depth: cutoff point for distance calculation.
+    """
+    # ignores these datasets to generate
     for dataset in datasets:
-        print(dataset)
+        if dataset in exclude:
+            continue
+
         G = nx.MultiDiGraph()
         file = Path(f"{dataset_path}/{dataset}/graph.txt").resolve()
         f = open(file)
@@ -23,15 +33,23 @@ def main():
             e2 = e2.strip()
             G.add_edge(e1, e2, key=r)
             G.add_edge(e2, e1, key=f"¬{r}")
-        
-        print(f"Calculating distances for {dataset}")
-        distances = dict(nx.all_pairs_shortest_path_length(G, 5))
-
-        for d in tqdm(distances.items()):
-            print(d)
 
         f.close()
         
+        print(f"Calculating distances for {dataset}, this may take a while...")
+        distances = dict(nx.all_pairs_shortest_path_length(G, max_depth))
+
+        cumsum = 0
+        for d in tqdm(distances.items()):
+            cumsum += len(d[1])
+
+        print(f"mapped {cumsum} distances for {dataset}")
+
+        with open(f"{cache_dir}/{dataset}.pkl", "wb") as f:
+            pickle.dump(distances, f)
+        
 
 if __name__ == "__main__":
-    main()
+    exclude = ["FB15K-237","KINSHIP", "UMLS", "WN18RR", "COUNTRIES"] 
+    max_depth = 3
+    main(exclude, max_depth)
