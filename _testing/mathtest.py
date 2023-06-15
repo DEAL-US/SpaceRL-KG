1a5703150469100d4e6fc2e2d4e46a44907c7b24
Miguel Bermudo <mig.ber.bay@gmail.com>
2023-06-15 13:07:39 +0000

    delete testing folder

--- _testing/mathtest.py
+++
@@ -1,7 +0,0 @@
-import ast
-
-lmao = "{'dataset': 'KINSHIP', 'models': ['TransE_l2'], 'use_gpu': True, 'regenerate_existing': False, 'normalize': True, 'add_inverse_path': True, 'fast_mode': False}"
-# lmao = lmao.replace("\'", "\"")
-# ey = json.loads(lmao)
-res = ast.literal_eval(lmao)
-print(res)
